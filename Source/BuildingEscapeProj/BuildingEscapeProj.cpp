af032cce78fb46b29714a99f9e9d5ed0648c9ae6
Hannah Newsome <h.e.newsome@gmail.com>
2016-08-20 18:34:57 -0700

    initial project setup commit

---
+++ Source/BuildingEscapeProj/BuildingEscapeProj.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscapeProj.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BuildingEscapeProj, "BuildingEscapeProj" );
---
+++ Source/BuildingEscapeProj/BuildingEscapeProjGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BuildingEscapeProjGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPEPROJ_API ABuildingEscapeProjGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
